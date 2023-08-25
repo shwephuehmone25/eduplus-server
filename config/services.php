ad3baed0cf8b66c2328944a07bc76bf3d56d1d7c
ShwePhue25 <shwephuehmone@ilbcedu.com>
2023-08-25 02:33:26 +0000

    edit google credentials & authorization

--- config/services.php
+++ config/services.php
@@ -34,7 +34,16 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
-        
+        'scopes' => [
+            \Google_Service_Calendar::CALENDAR_EVENTS_READONLY,
+            \Google_Service_Calendar::CALENDAR_READONLY,
+            \Google_Service_Oauth2::OPENID,
+            \Google_Service_Oauth2::USERINFO_EMAIL,
+            \Google_Service_Oauth2::USERINFO_PROFILE,
+        ],
+        'approval_prompt' => env('GOOGLE_APPROVAL_PROMPT', 'force'),
+        'access_type' => env('GOOGLE_ACCESS_TYPE', 'offline'),
+        'include_granted_scopes' => true,    
     ],
 
     'smspoh' => [
