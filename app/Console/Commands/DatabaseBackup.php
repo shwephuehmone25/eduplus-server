2fb7298a5da528c628f4ea009262e0663667aa8d
ShwePhue25 <shwephuehmone@ilbcedu.com>
2023-09-12 07:42:58 +0000

    add cron job for automatic Database Backup & add client_secrets.json file to gitignore

--- app/Console/Commands/DatabaseBackup.php
+++ app/Console/Commands/DatabaseBackup.php
@@ -1,12 +1,10 @@
 <?php
-
+  
 namespace App\Console\Commands;
-
+  
 use Illuminate\Console\Command;
-use Carbon\Carbon;
-use File;
-
-class DatabaseBackup extends Command
+  
+class DatabaseBackUp extends Command
 {
     /**
      * The name and signature of the console command.
@@ -14,43 +12,26 @@ class DatabaseBackup extends Command
      * @var string
      */
     protected $signature = 'database:backup';
-
+    
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create copy of mysql dump for existing database.';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
+    protected $description = 'Command description';
+  
     /**
      * Execute the console command.
-     *
-     * @return int
      */
-    public function handle()
+    public function handle(): void
     {
-        $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".sql";
-
-        // Create backup folder and set permission if not exist.
-        $storageAt = storage_path() . "/app/backup/";
-        if(!File::exists($storageAt)) {
-            File::makeDirectory($storageAt, 0755, true, true);
-        }
-
-        $command = "".env('DB_DUMP_PATH', 'mysqldump')." --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . $storageAt . $filename;
-
+        $filename = "backup-" . now()->format('Y-m-d') . ".gz";
+    
+        $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
+    
         $returnVar = NULL;
-        $output = NULL;
+        $output  = NULL;
+    
         exec($command, $output, $returnVar);
     }
 }
