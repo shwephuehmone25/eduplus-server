08d738ae6c58debacf6d105bcac81f8938b8be9b
ShinPhoo <138138366+ShinPhooWin88@users.noreply.github.com>
2023-11-06 13:07:29 +0630

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'https://uat.saungpokki.ilbc.edu.mm'],
 
     'allowed_origins_patterns' => [],
 
