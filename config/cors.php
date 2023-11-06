604948c84e603587e1c271b7b52a690bdeaf969c
shwephuehmone25 <shwephue7889@gmail.com>
2023-11-06 03:56:33 +0000

    update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'https://uat.saungpokki.ilbc.edu.mm'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
