e35897ca2f567dab81e2fc00ba612761c5268e9a
shwephuehmone25 <shwephue7889@gmail.com>
2023-11-16 08:27:43 +0000

    remove price column in module

--- database/migrations/2023_07_25_042500_create_ranks_table.php
+++ database/migrations/2023_07_25_042500_create_ranks_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('ranks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('price');
             $table->timestamps();
             $table->softDeletes();
         });
